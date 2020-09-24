de7754898c140476284b2506fb3f08c1fb9c293e
Achilles <68021552+lymanmcbride@users.noreply.github.com>
2020-09-24 10:42:11 -0600

    concatenated landmarks into a string and initiated greeting

--- skyroute.py
+++ skyroute.py
@@ -3,4 +3,16 @@ from vc_metro import vc_metro
 from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
-# Build your program below:
+#concatenate landmark_choices into a string
+landmark_string = ""
+for letter, landmark in landmark_choices.items():
+    landmark_string += "{} - {}\n".format(letter, landmark)
+
+def greet():
+    print("Hi there, and welcome to SkyRoute!\n We'll help you find the shortest route between the following Vancouver landmarks:\n" + landmark_string)
+
+#main program
+def skyroute():
+    greet()
+
+skyroute()
